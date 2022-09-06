711cbf78436268f5cbbd24848300f3a506d2daa6
petya3000 <99812822+petya3000@users.noreply.github.com>
2022-09-06 12:18:37 +0300

    Update LeapYearPrinter.java

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
-        System.out.println(year % 4 == 0 ? "leap" : "not leap");
+          if (year % 100 == 0) {
+                    if (year % 400 == 0) 
+                        System.out.println("leap");
+                    else
+                        System.out.println("not leap");
+                } else if (year % 4 == 0)
+                    System.out.println("leap");
+                else
+                    System.out.println("not leap");
     }
 }
